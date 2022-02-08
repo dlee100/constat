0928b8eddde4dd3f3320155b61b1328154a24b4f
dlee100 <danieljplee1@gmail.com>
2022-02-08 10:02:37 -0500

    edits to the script

--- conmon.sh
+++ conmon.sh
@@ -3,13 +3,18 @@
 # Run with `watch' command for constant monitoring: 
 # watch -n1 --color ./conmon.sh
 
+# Easier way to run this is to set the command above as an alias.
+# I also dropped this program in my /etc/ so that is why the text file is explicitly stated in the declare function.
+
 # Inputting data from text file and making it an associative array.
-declare -A ip $(cat ip_list.txt)
+declare -A ip $(cat /etc/con-mon/ip_list.txt)
 
 # Colors for text.
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
+
+echo "Current connection status of listed nodes in the LAN."
 
 # Loop through the array.
 for i in ${!ip[@]};
