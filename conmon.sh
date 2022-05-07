fc4f553ca512958454746eb4f249cec31e3ecccf
dlee100 <danieljplee1@gmail.com>
2022-05-06 20:24:10 -0400

    having output sorted alphabetically

--- conmon.sh
+++ conmon.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Run with `watch' command for constant monitoring: 
 # watch -n1 --color ./conmon.sh
 
 # Inputting data from text file and making it an associative array.
-declare -A ip $(cat ip_list.txt)
+declare -A ip $(cat ~/con-mon/ip_list.txt)
 
 # Colors for text.
 GREEN='\033[0;32m'
@@ -16,5 +16,5 @@ for i in ${!ip[@]};
 do 
     timeout 0.2 ping -c1 $i > /dev/null ;
     ( [[ $? -eq 0 ]] && echo -e "Connection to ${ip[$i]} .......... ${GREEN}SUCCESS${NC}" ) || echo -e "Connection to ${ip[$i]} .......... ${RED}FAILED${NC}" ;
-done | column -t
+done | sort | column -t
 
